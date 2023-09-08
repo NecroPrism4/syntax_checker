27aee3fff6cecef561ee5a9fd1246f31a5cf79f5
NecroPrism4 <alexhid14@gmail.com>
2023-09-07 18:09:32 -0700

    last changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,123 +3,132 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import {
-	TableData,
-	regexes,
-	checkType,
-	typeActions,
+  TableData,
+  regexes,
+  checkType,
+  typeActions,
+  reasignacionRegex,
 } from "./models/tableData.model";
 
 type Type$ = "int" | "string" | "date" | "boolean";
 
 function App() {
-	const [tableData, setTableData] = useState<TableData[]>([]);
-	const [textareaValue, setTextareaValue] = useState<string>("");
-	const [error, setError] = useState<boolean>(false);
+  const [tableData, setTableData] = useState<TableData[]>([]);
+  const [textareaValue, setTextareaValue] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
 
-	const handleInput = (code: string) => {
-		const lines: string[] = code.split("\n"); // Dividir el contenido en líneas
+  const handleInput = (code: string) => {
+    const lines: string[] = code.split("\n"); // Dividir el contenido en líneas
 
-		const matches = [];
+    const matches = [];
 
-		for (const line of lines) {
-			let type = checkType.exec(line);
-			let match;
+    for (const line of lines) {
+      const reassignMatch = reasignacionRegex.exec(line);
+      if (reassignMatch) {
+        const variable = reassignMatch[1];
+        const valor = reassignMatch[2];
+        for (const m of matches) {
+          if (m.variable == variable) m.value = valor;
+        }
+        setError(false);
+      } else {
+        let type = checkType.exec(line);
+        let match;
 
-			if (type !== null) match = regexes[type[0] as Type$].exec(line);
+        if (type !== null) match = regexes[type[0] as Type$].exec(line);
 
-			if (match !== null && match !== undefined) {
-				setError(false);
-				const type: string = match[1];
-				const variable = match[2];
-				let value = match[3];
+        if (match !== null && match !== undefined) {
+          setError(false);
+          const type: string = match[1];
+          const variable = match[2];
+          let value = match[3];
 
-				// Parsear el valor según el tipo
-				const parsedValue: string = typeActions[type as Type$](value);
+          // Parsear el valor según el tipo
+          const parsedValue: string = typeActions[type as Type$](value);
 
-				matches.push({ type, variable, value: parsedValue });
-			} else setError(true);
-		}
+          matches.push({ type, variable, value: parsedValue });
+        } else setError(true);
+      }
+    }
 
-		// Actualizar la tabla de valores con las variables encontradas
-		setTableData(matches as TableData[]);
-	};
+    // Actualizar la tabla de valores con las variables encontradas
+    setTableData(matches as TableData[]);
+  };
 
-	/* useEffect(() => {
+  /* useEffect(() => {
 		console.log(tableData);
 	}, [tableData]); */
 
-	return (
-		<div className='main'>
-			<div className='d-flex px-5'>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<div className='d-flex flex-direction-row gap-5 m-5'>
-				<div className='form-group inputArea gap-2 d-flex flex-column'>
-					<h6>Type here</h6>
-					<hr />
-					<div className='position-relative'>
-						<div className='line-numbers position-absolute'>
-							{/* Genera números de línea dinámicamente */}
-							{textareaValue.split("\n").map((_, index) => (
-								<div key={index}>{index + 1}</div>
-							))}
-						</div>
-						<textarea
-							className={`form-control px-5 ${error ? "is-invalid" : ""}`}
-							id='exampleFormControlTextarea1'
-							rows={20}
-							onChange={(e) => {
-								handleInput(e.target.value);
-								setTextareaValue(e.target.value);
-							}}
-							value={textareaValue}
-						></textarea>
-					</div>
-				</div>
-				<div className='actionArea gap-2 d-flex flex-column'>
-					<h6>Here is shown the value's table</h6>
-					<hr />
-					<div className='flex-grow-1'>
-						<table className='table table-striped'>
-							<thead>
-								<tr>
-									<th scope='col'>#</th>
-									<th scope='col'>type</th>
-									<th scope='col'>Variable</th>
-									<th scope='col'>Value</th>
-								</tr>
-							</thead>
-							<tbody>
-								{tableData.map((data, index) => (
-									<tr key={index}>
-										<th scope='row'>{index + 1}</th>
-										<td>{data.type}</td>
-										<td>{data.variable}</td>
-										<td>{data.value.toString()}</td>
-									</tr>
-								))}
-							</tbody>
-						</table>
-					</div>
-					<button
-						type='button'
-						className='btn btn-primary w-100 align-self-end'
-						onClick={() => {
-							setTableData([]);
-							setTextareaValue("");
-						}}
-					>
-						Clear
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="main">
+      <div className="d-flex px-5">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div className="d-flex flex-direction-row gap-5 m-5">
+        <div className="form-group inputArea gap-2 d-flex flex-column">
+          <h6>Type here</h6>
+          <hr />
+          <div className="position-relative">
+            <div className="line-numbers position-absolute">
+              {/* Genera números de línea dinámicamente */}
+              {textareaValue.split("\n").map((_, index) => (
+                <div key={index}>{index + 1}</div>
+              ))}
+            </div>
+            <textarea
+              className={`form-control px-5 ${error ? "is-invalid" : ""}`}
+              id="exampleFormControlTextarea1"
+              rows={20}
+              onChange={(e) => {
+                handleInput(e.target.value);
+                setTextareaValue(e.target.value);
+              }}
+              value={textareaValue}></textarea>
+          </div>
+        </div>
+        <div className="actionArea gap-2 d-flex flex-column">
+          <h6>Here is shown the value's table</h6>
+          <hr />
+          <div className="flex-grow-1">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">type</th>
+                  <th scope="col">Variable</th>
+                  <th scope="col">Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.map((data, index) => (
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{data.type}</td>
+                    <td>{data.variable}</td>
+                    <td>{data.value.toString()}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary w-100 align-self-end"
+            onClick={() => {
+              setTableData([]);
+              setTextareaValue("");
+            }}>
+            Clear
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/models/tableData.model.ts
+++ src/models/tableData.model.ts
@@ -1,25 +1,27 @@
 export interface TableData {
-	type: "int" | "string" | "date" | "boolean";
-	variable: string;
-	value: number | string | Date | boolean;
+  type: "int" | "string" | "date" | "boolean";
+  variable: string;
+  value: number | string | Date | boolean;
 }
 
 export const typeActions = {
-	int: (value: string) => parseInt(value, 10).toString(),
-	string: (value: string) => value.replace(/"/g, ""),
-	date: (value: string) => new Date(value.replace(/"/g, "")).toISOString(),
-	boolean: (value: string) => (value === "true").toString(),
+  int: (value: string) => parseInt(value, 10).toString(),
+  string: (value: string) => value.replace(/"/g, ""),
+  date: (value: string) => new Date(value.replace(/"/g, "")).toISOString(),
+  boolean: (value: string) => (value === "true").toString(),
 };
 
 export const checkType = /(string|int|boolean|date)/;
 
 // Expresión regular para buscar declaraciones de variable válidas en cada línea
 export const regexes = {
-	int: /^(int)\s+([a-zA-Z_]\w*)\s*=\s*(\d+)\s*;\s*$/,
-	string: /^(string)\s+([a-zA-Z_]\w*)\s*=\s*(".+")\s*;\s*$/,
-	date: /^(date)\s+([a-zA-Z_]\w*)\s*=\s*new Date\(".+"\)\s*;\s*$/,
-	boolean: /^(boolean)\s+([a-zA-Z_]\w*)\s*=\s*(true|false)\s*;\s*$/,
+  int: /^(int)\s+([a-zA-Z_]\w*)\s*=\s*(\d+)\s*;\s*$/,
+  string: /^(string)\s+([a-zA-Z_]\w*)\s*=\s*(".+")\s*;\s*$/,
+  date: /^(date)\s+([a-zA-Z_]\w*)\s*=\s*new Date\(".+"\)\s*;\s*$/,
+  boolean: /^(boolean)\s+([a-zA-Z_]\w*)\s*=\s*(true|false)\s*;\s*$/,
 };
+
+export const reasignacionRegex = /^\s*([a-zA-Z_]\w*)\s*=\s*(.*);/;
 
 /* export const expresionRegular =
 	/^(int|string|date|boolean)\s+([a-zA-Z_]\w*)\s*=\s*(\d+|".*"|new Date\(".+"\)|true|false)\s*;\s*$/g; */
